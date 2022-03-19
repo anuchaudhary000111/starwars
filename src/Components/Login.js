c7bfa29e97248aeeee75784c29619c6ce30e3d2f
Anu Chaudhary <anuchaudhary000111@gmail.com>
2022-03-19 21:09:39 +0000

    firstcommit

---
+++ src/Components/Login.js
@@ -0,0 +1,70 @@
+import "./login.css";
+import star from "../images/star.png";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [userName, changeUserName] = useState("");
+  const [userPassword, changeUserPassword] = useState([]);
+  const [userCheckBox, changeUserCheckBox] = useState(true);
+
+  const doLogin = () => {
+    axios
+      .get("https://swapi.dev/api//people/?search=" + userName)
+      .then((response) => {
+        if (response.data.results.length === 0) {
+          alert("incorrect data");
+        } else {
+          let found = false;
+          response.data.results.map((data) => {
+            if (data.birth_year === userPassword) {
+              found = true;
+            }
+          });
+          if (found) {
+            alert("Successfully login");
+          } else {
+            alert("incorrect data");
+          }
+        }
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="headr">
+      <img src={star} />
+      <div className="head">
+        <p>
+          Username
+          <input
+            value={userName}
+            onChange={(e) => changeUserName(e.target.value)}
+          />
+        </p>
+        <p>
+          Password{" "}
+          <input
+            value={userPassword}
+            onChange={(e) => changeUserPassword(e.target.value)}
+          />
+        </p>
+        <p>
+          <input
+            type="checkBox"
+            value={userCheckBox}
+            onChange={(e) => changeUserCheckBox(e.target.value)}
+          />
+        </p>
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={doLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
